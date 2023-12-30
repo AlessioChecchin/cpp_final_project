6a23153278e8baf77ab7a4e8db160d8284e30baa
AlessioChecchin <alessiochecchin@gmail.com>
2023-12-30 22:05:16 +0100

    Config class

--- include/config.h
+++ include/config.h
@@ -1,19 +1,176 @@
+/**
+ * @author Alessio Checchin
+*/
+
 #ifndef CONFIG_H
 
-class config
+#include <map>
+#include <vector>
+#include <string>
+
+namespace prj
 {
-protected:
-	int buy_prices[][] =
+	/**
+	 * This class contains all the information necessary to correctly configure the game.
+	 * It also allows convenient customization by a potential user, who can provide
+	 * different initialization techniques (for example, a subclass can load all necessary
+	 * information from a config. file).
+	 * 
+	 * @author Alessio Checchin
+	 * @version 1.0.0
+	*/
+	class config
 	{
-		{ 6, 10, 20 },
-		{ 3,  5, 10 },
-		{ 3,  5, 10 }
-	};
+	protected:
 
-	int stay_prices[][] =
-	{
+		/**
+		 * This matrix contains all the prices applied when buying or upgrading a building or terrain.
+		 * 
+		 * The default settings are:
+		 * BUILDING      | ECONOMY       | STANDARD      | LUXURY
+		 * ------------- | ------------- | ------------- | -------------
+		 * TERRAIN       | 6             | 10            | 20
+		 * HOUSE         | 3             | 5             | 10
+		 * HOTEL         | 3             | 5             | 10 
+		*/
+		std::vector<std::vector<unsigned int>> buy_prices_{};
+
+		/**
+		 * This matrix contains all the prices applied when staying in a building.
+		 * 
+		 * The default settings are:
+		 * BUILDING      | ECONOMY       | STANDARD      | LUXURY
+		 * ------------- | ------------- | ------------- | -------------
+		 * HOUSE         | 2             | 4             | 7
+		 * HOTEL         | 4             | 8             | 14 
+		*/
+		std::vector<std::vector<unsigned int>> stay_prices_{};
+
+		/**
+		 * The number of bots playing.
+		 * Default value: 3.
+		*/
+		unsigned int bot_number_{};
 		
-	}
-};
+		/**
+		 * The initial budget of each player.
+		 * Default value: 100.
+		*/
+		unsigned int initial_budget_{};
+		
+		/**
+		 * The amount of bucks given to each player when a full cycle is completed.
+		 * Default value: 20.
+		*/
+		unsigned int bonus_cycle_{};
 
-#define CONFIG_H // CONFIG_H
+		/**
+		 * The number of dices that each player throws each round.
+		 * Default value: 2.
+		*/
+		unsigned int dice_number_{};
+
+		/**
+		 * The number of faces of each dice.
+		 * Default value: 6.
+		*/
+		unsigned int dice_faces_number_{};
+		
+		/**
+		 * Chance of a bot buying land.
+		 * Default value: 0.25.
+		*/
+		double buy_land_possibility_bot_{};
+
+		/**
+		 * Chance of a bot buying a house.
+		 * Default value: 0.25.
+		*/
+		double buy_house_possibility_bot_{};
+
+		/**
+		 * Chance of a bot buying a hotel.
+		 * Default value: 0.25.
+		*/
+		double buy_hotel_possibility_bot_{};
+
+		/**
+		 * Symbols and texts used for displaying informations to the user.
+		 * SYMBOL NAME   | VALUE       
+		 * ------------- | ------------- 
+		 * box_angular   | <space>
+		 * box_start     | "P"
+		 * box_economy   | "E"
+		 * box_standard  | "S"
+		 * box_luxury    | "L"
+		 * suf_house     | "*"
+		 * suf_hotel     | "^"
+		 * suf_player    | "%d"
+		*/
+		std::map<std::string, std::string> display_props_{};
+
+		/**
+		 * Path to the file that stores log infos.
+		*/
+		std::string log_path_{};
+
+	public:
+	
+		/**
+		 * Bot number getter.
+		 * @return The number of bots in the game.
+		*/
+		virtual unsigned int get_bot_number() const;
+
+		/**
+		 * Initial budget getter.
+		 * @return The initial budget of each player.
+		*/
+		virtual unsigned int get_initial_budget() const;
+
+		/**
+		 * Bonus cycle getter.
+		 * @return The amount of bucks given to each player when a full cycle is completed.
+		*/
+		virtual unsigned int get_bonus_cycle() const;
+
+		/**
+		 * Dice number getter.
+		 * @return The number of dices that each player throws each round.
+		*/
+		virtual unsigned int get_dice_number() const;
+
+		/**
+		 * Dice faces number getter.
+		 * @return The number of faces of each dice.
+		*/
+		virtual unsigned int get_dice_faces_number() const;
+
+		/**
+		 * Buy land possibility getter.
+		 * @return Chance of a bot buying a land.
+		*/
+		virtual double get_buy_land_possibity_bot() const;
+
+		/**
+		 * Buy house possibility getter.
+		 * @return Chance of a bot buying a house.
+		*/
+		virtual double get_buy_house_possibility_bot() const;
+
+		/**
+		 * Buy hotel possibility getter.
+		 * @return Chance of a bot buying a hotel.
+		*/
+		virtual double get_buy_hotel_possibility_bot() const;
+
+		/**
+		 * Log path getter.
+		 * @return Path to the file that stores log infos.
+		*/
+		virtual std::string get_log_path() const;
+	};
+}
+
+
+#endif CONFIG_H // CONFIG_H
---
+++ src/config.cpp
@@ -0,0 +1,81 @@
+#include "config.h"
+
+namespace prj
+{
+	config::config(): 
+		buy_prices_{
+			{
+				{ 6, 10, 20 },
+				{ 3,  5, 10 },
+				{ 3,  5, 10 }
+			}
+		},
+		stay_prices_{
+			{ 2, 4,  7 },
+			{ 4, 8, 14 },
+		},
+		bot_number_{3},
+		initial_budget_{100},
+		bonus_cycle_{20},
+		dice_number_{2},
+		dice_faces_number_{6},
+		buy_land_possibility_bot_{0.25},
+		buy_house_possibility_bot_{0.25},
+		buy_hotel_possibility_bot_{0.25},
+		log_path_{"./log.txt"}
+	{
+		display_props_["box_angular"]  = " ";
+		display_props_["box_start"]    = "P";
+		display_props_["box_economy"]  = "E";
+		display_props_["box_standard"] = "S";
+		display_props_["box_luxury"]   = "L";
+		display_props_["suf_house"]    = "*";
+		display_props_["suf_hotel"]    = "^";
+		display_props_["suf_player"]   = "%d";
+	}
+
+	unsigned int config::get_bot_number() const
+	{
+		return bot_number_;
+	}
+
+	unsigned int config::get_initial_budget() const
+	{
+		return initial_budget_;
+	}
+
+	unsigned int config::get_bonus_cycle() const
+	{
+		return bonus_cycle_;
+	}
+
+	unsigned int config::get_dice_number() const
+	{
+		return dice_number_;
+	}
+
+	unsigned int config::get_dice_faces_number() const
+	{
+		return dice_faces_number_;
+	}
+
+	double config::get_buy_land_possibity_bot() const
+	{
+		return buy_land_possibility_bot_;
+	}
+
+	double config::get_buy_house_possibility_bot() const
+	{
+		return buy_house_possibility_bot_;
+	}
+
+	double config::get_buy_hotel_possibility_bot() const
+	{
+		return buy_hotel_possibility_bot_;
+	}
+
+	std::string config::get_log_path() const
+	{
+		return log_path_;
+	}
+}
