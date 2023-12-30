00126059ea4b9f64b35f64545de7724ef0210aa2
Klementina Ylli <klemeylli@gmail.com>
2023-12-30 16:23:26 +0100

    Beta config

---
+++ include/config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H
+
+class config
+{
+protected:
+	int buy_prices[][] =
+	{
+		{ 6, 10, 20 },
+		{ 3,  5, 10 },
+		{ 3,  5, 10 }
+	};
+
+	int stay_prices[][] =
+	{
+		
+	}
+};
+
+#define CONFIG_H // CONFIG_H
