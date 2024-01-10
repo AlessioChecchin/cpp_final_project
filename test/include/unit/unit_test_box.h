6effddc49d795f0a8a7eaf39bf358884ebfec9c7
Giulio Cattelan <115191745+GC-03@users.noreply.github.com>
2024-01-10 17:25:58 +0100

    unit_test_boox added. It's still empty

---
+++ test/include/unit/unit_test_box.h
@@ -0,0 +1,22 @@
+/**
+ * Unit test for box class.
+ * 
+ * @author Giulio Cattelan
+*/
+
+#ifndef UNIT_TEST_BOX_H
+#define UNIT_TEST_BOX_H
+
+#include "core/test_suite.h"
+
+class unit_test_box: public test_suite
+{
+public:
+
+	unit_test_box();
+
+	void test_constructor();
+	void test_getters();
+};
+
+#endif // UNIT_TEST_BOX_H
