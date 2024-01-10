0e1e1d73c329e996991c8e4b14b6a8aed6d47b58
Giulio Cattelan <115191745+GC-03@users.noreply.github.com>
2024-01-10 19:46:03 +0100

    unit_test_game added

---
+++ test/src/unit/unit_test_game.cpp
@@ -0,0 +1,32 @@
+#include "unit/unit_test_game.h"
+#include "../../include/game.h"
+#include "../../include/config.h"
+
+#include <iostream>
+#include <assert.h>
+
+unit_test_game::unit_test_game(): test_suite{"unit_test_game"}
+{
+	// Binding method. Based on the current test structure
+	// we are sure that test object does not outlive the current class instance.
+	add_test(test("constructor", std::bind(&unit_test_game::test_constructor, this)));
+}
+
+void unit_test_game::test_constructor()
+{
+	//testing constructor
+	assert(("Failed to create game object", test_game_valid()));
+}
+
+bool test_game_valid()
+{
+	try
+	{
+		prj::game a(std::shared_ptr<prj::config>(new prj::config()));
+		return true;
+	}
+	catch(const std::exception& e)
+	{
+		return false;
+	}
+}
