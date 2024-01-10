9b8f7e781f73285f2423c6847b7a27369e162bf1
Giulio Cattelan <115191745+GC-03@users.noreply.github.com>
2024-01-10 12:53:41 +0100

    assert comments modified in test_category_config in unit_test_config

--- test/src/unit/unit_test_config.cpp
+++ test/src/unit/unit_test_config.cpp
@@ -24,19 +24,19 @@ void unit_test_config::test_category_config()
 	//testing categry__coonfig constructor
 	//note: there is an implicit cast when comparing an unsigned int with a negative int
 	prj::config::category_config a{"a", 0, 8};
-	assert(("Failed to build the category_config:", a.name == "a"));
-	assert(("Failed to build the category_config:", a.id == 0));
-	assert(("Failed to build the category_config:", a.number == 8));
+	assert(("Failed to create category_config object a{\"a\", 0, 8}:", a.name == "a"));
+	assert(("Failed to create category_config object a{\"a\", 0, 8}:", a.id == 0));
+	assert(("Failed to create category_config object a{\"a\", 0, 8}:", a.number == 8));
 
 	prj::config::category_config b{"abcdefg", 2, -203};
-	assert(("Failed to build the category_config:", b.name == "abcdefg"));
-	assert(("Failed to build the category_config:", b.id == -1));
-	assert(("Failed to build the category_config:", b.number == -203));
+	assert(("Failed to create category_config object b{\"abcdefg\", 2, -203}:", b.name == "abcdefg"));
+	assert(("Failed to create category_config object b{\"abcdefg\", 2, -203}:", b.id == -1));
+	assert(("Failed to create category_config object b{\"abcdefg\", 2, -203}:", b.number == -203));
 
-	prj::config::category_config a{"", 0, 8};
-	assert(("Failed to build the category_config:", a.name == ""));
-	assert(("Failed to build the category_config:", a.id == 0));
-	assert(("Failed to build the category_config:", a.number == 8));
+	prj::config::category_config c{"", 0, 8};
+	assert(("Failed to create category_config object c{\"\", 0, 8}:", c.name == ""));
+	assert(("Failed to create category_config object c{\"\", 0, 8}:", c.id == 0));
+	assert(("Failed to create category_config object c{\"\", 0, 8}:", c.number == 8));
 	
 	
 }
