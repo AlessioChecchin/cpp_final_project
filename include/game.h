1457adfa808110b8d0e2d2852b890a81e9eca860
jacopix <jacopo@studiotoniolo.eu>
2024-01-13 01:02:44 +0100

    added all log functions in game.cpp / game.h

--- include/game.h
+++ include/game.h
@@ -10,6 +10,7 @@
 #include "config.h"
 #include "playground.h"
 #include "dice.h"
+#include "logger/logger.h"
 
 namespace prj
 {
@@ -19,17 +20,8 @@ class game
         std::shared_ptr<config> conf_;
         playground playgr_;
         std::vector<dice> dices;
-
-    public:
-        /**
-         * Default constructor 
-         */
-        game(std::shared_ptr<config> conf);
-
-        /**
-         * Create players
-         */
-        std::multimap<unsigned long int , std::shared_ptr<player>, std::greater<unsigned long int>>  create_players(unsigned int init_balance);
+        logger& logger_;
+        unsigned int last_roll_;
 
         /**
          * Order players with dice rolling
@@ -38,11 +30,94 @@ class game
         void order_players(std::multimap<unsigned long int , std::shared_ptr<player>, std::greater<unsigned long int>>& players);
 
         /**
-         * Roll dices and get their value
+         * Create players
+         */
+        std::multimap<unsigned long int , std::shared_ptr<player>, std::greater<unsigned long int>>  create_players(unsigned int init_balance);
+
+        /**
+         * Roll dices and save their value
          * @param dice_number The number of dices to roll
          * @return unsigned long int The total score 
          */
         unsigned long int roll_dice();
+
+        /**
+         * Get the last dice roll
+         * @return Last roll value
+         */
+        unsigned long int last_roll();
+
+        /**
+         * 
+         * @param p Target Player
+         */
+        void log_start_bonus(std::shared_ptr<player> p) const;
+
+        /**
+         * Logs "dice rolled" event
+         * @param p Target Player
+         * @param score 
+         */
+        void log_dice_rolled(std::shared_ptr<player> p, int score) const;
+
+        /**
+         * Logs "player arrived at position" event
+         * @param p Target Player
+         */
+        void log_arrived(std::shared_ptr<player> p) const;
+
+        /**
+         * Logs "player bought house" event
+         * @param p Target Player
+         */
+        void log_bought_house(std::shared_ptr<player> p) const;
+
+        /**
+         * Logs "player bought terrain" event
+         * @param p Target Player
+         */
+        void log_bought_terrain(std::shared_ptr<player> p) const;
+
+        /**
+         * Logs "player bought hotel" event
+         * @param p Target Player
+         */
+        void log_bought_hotel(std::shared_ptr<player> p) const;
+
+        /**
+         * Logs "payer paid amount to payee" event
+         * @param payee Player who's receving the amount
+         * @param payer Player who's paying the amount
+         * @param amount The amount of money
+         */
+        void log_fee(std::shared_ptr<player> payee, std::shared_ptr<player> payer, unsigned int amount) const;
+
+        /**
+         * Logs "round ended" event
+         * @param p Target Player
+         */
+        void log_round_ended(std::shared_ptr<player> p) const;
+
+        /**
+         * Logs "player eliminated" event
+         * @param p Target Player
+         */
+        void log_eliminated(std::shared_ptr<player> p) const;
+
+        /**
+         * Logs "player won" event
+         * @param p Target Player
+         */
+        void log_win(std::shared_ptr<player> p) const;
+
+
+
+    public:
+        /**
+         * Default constructor 
+         */
+        game(std::shared_ptr<config> conf);
+
         
 
 };
--- src/game.cpp
+++ src/game.cpp
@@ -10,12 +10,13 @@
 #include "players/bot.h"
 #include "players/human.h"
 #include "game.h"
+#include "logger/logger.h"
 namespace prj
 {
-game::game(std::shared_ptr<config> conf): conf_{conf}, playgr_{conf} 
+game::game(std::shared_ptr<config> conf): conf_{conf}, playgr_{conf}, logger_{logger_.get_logger()}
 {
 
-	std::cout << playgr_;
+	std::cout << playgr_ << std::endl;   
     // Create players with initial budget
     auto tempPlayers = create_players(conf_->get_initial_budget());
 
@@ -23,12 +24,14 @@ game::game(std::shared_ptr<config> conf): conf_{conf}, playgr_{conf}
     order_players(tempPlayers);
     auto players = playgr_.get_players();
 
+    //TEST
+    for(int i=0; i<players.size(); i++)
+        std::cout<<"Player "<<players[i]->get_id()<<std::endl;
+
     // Game continues until someone wins or it's a bot-game (with max number of rounds)
     // Rounds are in range [1, conf_->round_number]
     bool game_end = false;
-    unsigned int round_counter = 0;
-
-    
+    unsigned int round_counter = 0;    
 
     while(!game_end)
     {  
@@ -39,11 +42,14 @@ game::game(std::shared_ptr<config> conf): conf_{conf}, playgr_{conf}
         // Move player
         playgr_.move_player(current_player, roll_dice());
 
+
+
         // Per test - da togliere
         game_end = true;
         ////////////////////////////////////////////////////////////////////////////////////////v
-        // It's a bot-only game
-        if(conf_->get_human_number()==0 && round_counter==conf_->get_round_number())
+
+        // ------ Bisogna tenere i turni max anche per partite human-bot
+        if(round_counter==conf_->get_round_number())
             game_end = true;
 
         // It's a human or bot-only game
@@ -77,6 +83,7 @@ void game::order_players(std::multimap<unsigned long int , std::shared_ptr<playe
             if(is_copying)
             {
                 tempMap.insert({roll_dice(), prevIt->second});
+                log_dice_rolled(prevIt->second, last_roll());
                 is_copying = false;
                 
                 // Check if second dice_roll succeeded
@@ -96,6 +103,7 @@ void game::order_players(std::multimap<unsigned long int , std::shared_ptr<playe
         else if(prevIt->first == it->first)
         {
             tempMap.insert({roll_dice(), prevIt->second});
+            log_dice_rolled(prevIt->second, last_roll());
             is_copying = true;
         }       
 
@@ -107,14 +115,24 @@ void game::order_players(std::multimap<unsigned long int , std::shared_ptr<playe
 std::multimap<unsigned long int , std::shared_ptr<player>, std::greater<unsigned long int>> game::create_players(unsigned int init_balance)
 {
     std::multimap<unsigned long int , std::shared_ptr<player>, std::greater<unsigned long int>> players;
-
+    
     // Add bots to playground
     for(int i=0 ; i<conf_->get_bot_number(); i++)
-        players.insert({roll_dice(),std::shared_ptr<player>(new bot(init_balance))});
+    {
+        std::shared_ptr<player> player(new bot(init_balance));
+        log_dice_rolled(player, roll_dice());
+
+        players.insert({last_roll(), player});
+    }
     
     // Add human player to playground
     for(int i=conf_->get_bot_number(); i<conf_->get_human_number(); i++)
-        players.insert({roll_dice(),std::shared_ptr<player>(new human(init_balance))});
+    {
+        std::shared_ptr<player> player(new human(init_balance));
+        
+        log_dice_rolled(player, roll_dice());
+        players.insert({last_roll(), player});
+    }
     
     return players; 
 }
@@ -127,11 +145,101 @@ unsigned long int game::roll_dice()
             dices.push_back(std::move(dice(6)));
     
     // Roll and sum the results
-    unsigned long int score = 0;
+    last_roll_ = 0;
     for(int i=0; i<dices.size(); i++)
-       score += dices[i].roll();
+       last_roll_ += dices[i].roll();
 
-    return score;
+    return last_roll();
+}
+
+unsigned long int game::last_roll()
+{
+    return last_roll_;
+}
+
+
+void game::log_start_bonus(std::shared_ptr<player> p) const
+{
+    logger_ << "Giocatore "
+        << p->get_id() 
+        << " e' passato dal via e ha ritirato "
+        << conf_->get_bonus_cycle()
+        << " fiorini"
+        << std::endl;
+}
+void game::log_dice_rolled(std::shared_ptr<player> p, int score) const
+{
+    logger_ << "Giocatore "
+            << p->get_id() 
+            << " ha tirato i dadi ottenendo un valore di "
+            << score 
+            << std::endl;
+
+}
+void game::log_arrived(std::shared_ptr<player> p) const
+{
+    logger_ << "Giocatore "
+            << p->get_id() 
+            << " e' arrivato alla casella "
+            << p->get_pos()
+            << std::endl;
+}
+void game::log_bought_house(std::shared_ptr<player> p) const
+{
+    logger_ << "Giocatore "
+            << p->get_id() 
+            << " ha costruito una casa sul terreno "
+            << p->get_pos()
+            << std::endl;
+}
+void game::log_bought_terrain(std::shared_ptr<player> p) const
+{
+    logger_ << "Giocatore "
+            << p->get_id() 
+            << " ha acquistato il terreno "
+            << p->get_pos()
+            << std::endl;
+}
+void game::log_bought_hotel(std::shared_ptr<player> p) const
+{
+    logger_ << "Giocatore "
+            << p->get_id() 
+            << " ha migliorato una casa in albergo sul terreno "
+            << p->get_pos()
+            << std::endl;
+}
+void game::log_fee(std::shared_ptr<player> payee, std::shared_ptr<player> payer, unsigned int amount) const
+{
+    logger_ << "Giocatore "
+            << payer->get_id() 
+            << " ha pagato "
+            << amount
+            << " fiorini a giocatore "
+            << payee->get_id()
+            << " per pernottamento nella casella "
+            << payer->get_pos()
+            << std::endl;
+}
+void game::log_round_ended(std::shared_ptr<player> p) const
+{
+    logger_ << "Giocatore "
+            << p->get_id() 
+            << " ha finito il turno"
+            << std::endl;
+}
+void game::log_eliminated(std::shared_ptr<player> p) const
+{
+    logger_ << "Giocatore "
+            << p->get_id() 
+            << " e' stato eliminato"
+            << std::endl;
+}
+void game::log_win(std::shared_ptr<player> p)  const
+{
+    logger_ << "Giocatore "
+            << p->get_id() 
+            << " ha vinto la partita"
+            << std::endl;
 }
 
 }   
